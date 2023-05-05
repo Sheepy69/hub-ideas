cd9ad5260baa8d6b4a8cba931285dd03725de6f7
Thomas Moutlen <thomas.moutlen@ph2m.com>
2023-05-05 12:47:17 +0200

    Init back package

---
+++ back/src/Model/Database/Db.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace HubIdeas\Back\Database;
+
+class Db
+{
+    public function __construct()
+    {
+        $this->init();
+    }
+
+    protected function init(): void
+    {
+
+    }
+}
